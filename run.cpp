057a6ad0bf71c8f1c6515053ea0f7f2028aacf19
kamiokan <spherenov@gmail.com>
2019-08-13 00:12:11 +0900

    string型で検索してみた

--- run.cpp
+++ run.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
+// #include<algorithm> std::find()と同等の機能
 template<class Iterator, class T>
 Iterator my_find(Iterator first, Iterator last, T value) {
     while (first != last) {
@@ -20,6 +22,15 @@ int main() {
     decltype(v)::iterator it = my_find(v.begin(), v.end(), 3);
     if (it != v.end()) {
         cout << *it << endl;
+    } else {
+        cout << "not found!" << endl;
+    }
+
+    // ももクロから佐々木彩夏を検索する
+    vector<string> momokuro = {"百田夏菜子", "玉井詩織", "佐々木彩夏", "高城れに"};
+    decltype(momokuro)::iterator it2 = my_find(momokuro.begin(), momokuro.end(), "佐々木彩夏");
+    if (it2 != momokuro.end()) {
+        cout << *it2 << endl;
     } else {
         cout << "not found!" << endl;
     }
